5ecd5e9a4cb1b24f39525b90076f302825b8ec17
Suryash-Jha <joblesscoder2468@gmail.com>
2025-05-18 18:47:51 +0530

    tracking the retrieve api count also

--- backend/middleware/checkRedisCount.js
+++ backend/middleware/checkRedisCount.js
@@ -3,25 +3,25 @@ const configData = require('../limiterConfig.json')
 const redis = new Redis()
 const RedisLimiterMiddleware = async (req, res, next) => {
     console.log(req.path, '--->req.path')
-    if(req.path=== '/retrive-redis-data') {
-        return next()
-    }
+    // if(req.path=== '/retrive-redis-data') {
+    //     return next()
+    // }
     let keyName = ''
     if (configData.limitingType === 'ip_based')
         keyName = req.ip + ':' + req.path
     else
         keyName = req.appName + ':' + req.path
     const callsCount = await redis.get(keyName)
-    if (callsCount >= configData.limitCount) {
+    if (callsCount >= configData.limitCount && req.path !== '/retrive-redis-data') {
         res.status(429).send({
             message: 'API LIMIT REACHED!! Please Wait'
         })
     }
 
-        await redis.multi()
-            .incr(keyName)
-            .expire(keyName, configData.limitDuration)
-            .exec()
+    await redis.multi()
+        .incr(keyName)
+        .expire(keyName, configData.limitDuration)
+        .exec()
 
 
     next()
